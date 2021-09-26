d1bb4917d53814526dbdef9820fb27be90f6a527
minadolf <duaconthannuoc@gmail.com>
2021-09-26 06:59:10 +0200

    traitim

---
+++ config.js
@@ -0,0 +1,23 @@
+const CONFIG = {
+    introTitle: 'Chào người đẹp',
+    introDesc: `Hãy ấn vào nút bên dưới để tiếp tục`,
+    btnIntro: 'Tiếp tục',
+    title: 'Chị có đồng ý cưới em không?',
+    desc: 'Hãy trả lời thật lòng nhé',
+    btnYes: 'Có',
+    btnNo: 'Không',
+    question:'Nếu chị đã thật lòng như vậy thì em xin nhận tình yêu của chị mà hãy nói cho em biết tại sao chị lại yêu em?',
+    btnReply: 'Xác nhận câu trả lời là thật lòng',
+    reply: 'Nếu chị đã thật lòng như vậy thì hãy nhắn tin cho em để hỏi cưới nhé',
+    mess: 'Nếu chị đã thật lòng như vậy thì hãy nhắn tin cho em để hỏi cưới nhé',
+    messDesc: 'Sau lần trả lời này sẽ không được trả lời lại đâu',
+    btnAccept: 'Đồng ý',
+    messLink: 'https://www.facebook.com/adolf.min.9/', //link fb
+    pr: 'bản quyền thuộc về minadolf',
+    bgLink: 'https://scontent-mxp1-1.xx.fbcdn.net/v/t1.6435-9/159448742_1922985061200989_5926807812615073955_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=8LhOdlS5mkwAX8upXrR&tn=c3en8e_Zdk9eJhkH&_nc_ht=scontent-mxp1-1.xx&oh=1c2e57194e218cfcb06d9199fd43be67&oe=6173DD24', //link ảnh nền
+    iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
+    lookMeLink: 'https://scontent-mxp1-1.xx.fbcdn.net/v/t1.6435-9/159448742_1922985061200989_5926807812615073955_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=8LhOdlS5mkwAX8upXrR&tn=c3en8e_Zdk9eJhkH&_nc_ht=scontent-mxp1-1.xx&oh=1c2e57194e218cfcb06d9199fd43be67&oe=6173DD24', //link ảnh lookMe
+    soundLink: 'https://www.youtube.com/watch?v=o_lN37OAJ9U', //link nhac
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
